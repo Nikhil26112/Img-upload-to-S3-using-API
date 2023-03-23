81ca3d9073a9bc6d73205d23d2fa077170c57b83
Nikhil <nikhilchauhan2611@gmail.com>
2023-03-23 15:40:42 +0530

    nikhil ec2 instance added

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_key_pair" "my-key" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_instance" "My-EC2-Instance" {
+resource "aws_instance" "Nikhil-EC2-Instance" {
   ami           = "ami-0f8ca728008ff5af4"
   instance_type = "t2.micro"
   key_name      = "my-key"
@@ -16,7 +16,7 @@ resource "aws_instance" "My-EC2-Instance" {
     aws_security_group.allow_ssh.id
   ]
   tags = {
-    Name = "My EC2 Instance"
+    Name = "Nikhil's EC2 Instance"
   }
 }
 
@@ -68,5 +68,5 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 output "ec2_global_ips" {
-  value = ["${aws_instance.My-EC2-Instance.*.public_ip}"]
+  value = ["${aws_instance.Nikhil-EC2-Instance.*.public_ip}"]
 }
